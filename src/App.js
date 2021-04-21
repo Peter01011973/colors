666644fd0f66f23460fcc76a2ae59f4a908a3e67
Peter <petr_adamovich@i.ua>
2021-04-21 21:31:29 +0300

    functional component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import './App.scss';
-import {Component, createRef} from 'react'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    // this.start = this.start.bind(this);
+import {useState, createRef} from 'react'
 
-    this.boxesCount = 50;
-    this.boxesRef = [];
-    for (let i=0; i<this.boxesCount; i++) this.boxesRef.push(createRef());
-    console.log('1: ',this.boxesRef);
-  }
+const App = () => {
 
-  state =  {
-    running: true
-  }
+  const boxesCount = 50;
+  const boxesRef = [];
+  for (let i=0; i<boxesCount; i++) boxesRef.push(createRef());
+  console.log('1: ',boxesRef);
 
-  getRandomColor() {
+  const [running, setRunning] =  useState(true);
+
+  const getRandomColor = () => {
     let letters = "0123456789ABCDEF"
     let color = '#'
     for (let i = 0; i < 6; i++){
@@ -25,29 +20,27 @@ class App extends Component {
     return color
   }
 
-  start = () => {
-    if(this.state.running) {
-      this.boxesRef.forEach(boxRef =>{
+  const start = () => {
+    if(running) {
+      boxesRef.forEach(boxRef =>{
         console.log('boxRef', boxRef);
         setInterval(
-          () => boxRef.current.style.background = this.getRandomColor(), 1000
+          () => boxRef.current.style.background = getRandomColor(), 1000
         )
       })
     }
   }
   
-  render() {
-    return (
-      <div>
-        <div className="box-container">
-          { this.boxesRef.map((boxRef, index) =>(<div ref={boxRef} className="box" key = {index}>{index}</div>)) }
-        </div>
-        <div className="container">
-          <button onClick={this.start} className="btn" id='btn-1'>Change Color</button>
-        </div>
+  return (
+    <div>
+      <div className="box-container">
+        { boxesRef.map((boxRef, index) =>(<div ref={boxRef} className="box" key = {index}>{index}</div>)) }
       </div>
-      );
-    }
-}
+      <div className="container">
+        <button onClick={start} className="btn" id='btn-1'>Change Color</button>
+      </div>
+    </div>
+    );
+  }
 
 export default App;
